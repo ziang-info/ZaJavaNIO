0886d3b07cfbbba9c3ec127f82a6b0f8da7f998f
Lattimore Lau <sunnyjava@hotmail.com>
2019-06-30 12:19:24 +0800

    ZaSocketNIO

---
+++ src/main/java/info/ziang/nio/ZaSocketClientNIO.java
@@ -0,0 +1,55 @@
+package info.ziang.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+public class ZaSocketClientNIO {
+
+    // 客户端标记
+    private final String clientId = UUID.randomUUID().toString();
+
+    public static void main(String[] args) {
+        new ZaSocketClientNIO().client();
+    }
+
+    /**
+     * 客户端采用NIO实现
+     */
+    public void client() {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        SocketChannel socketChannel = null;
+        try {
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            socketChannel.connect(new InetSocketAddress("192.168.31.103", 8080));
+            if (socketChannel.finishConnect()) {
+                int i = 0;
+                while (true) {
+                    TimeUnit.SECONDS.sleep(1);
+                    String info = "I'm " + i++ + "-th information from client (" + clientId + ")";
+                    buffer.clear();
+                    buffer.put(info.getBytes());
+                    buffer.flip();
+                    while (buffer.hasRemaining()) {
+                        System.out.println(buffer);
+                        socketChannel.write(buffer);
+                    }
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (socketChannel != null) {
+                    socketChannel.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
